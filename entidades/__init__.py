02333196c9bd7d7af2bf4d695d3df9b22dacbfcc
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-06-09 18:58:36 -0300

    feat: includes the imports on the __init__.py file of the entidades module

---
+++ entidades/__init__.py
@@ -0,0 +1,6 @@
+from plataforma import Plataforma
+from conteudo import Conteudo
+from usuario import Usuario
+from interacao import Interacao
+
+__all__ = ['Plataforma', 'Conteudo', 'Usuario', 'Interacao']
