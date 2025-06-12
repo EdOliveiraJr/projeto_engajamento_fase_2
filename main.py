130f39f1e601ad802317ce1c1a835cb5b907e5cb
Felipe Martins <xfelipexmartins@gmail.com>
2025-06-12 17:16:31 -0300

    Incluir a chamada cadastrar plataforma no arquivo main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from analise import Sistema
 if __name__ == '__main__':
 	sistema = Sistema()
 	sistema.processar_interacoes_do_csv('interacoes_globo.csv')
-    
+	sistema.cadastrar_plataforma('interacoes_globo.csv')
