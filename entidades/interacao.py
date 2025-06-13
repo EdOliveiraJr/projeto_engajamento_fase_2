27a7e67a393e3aa09a8d4245512538b7b99c526a
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-06-13 10:36:12 -0300

    refactor: remover atributo interacao_id da classe Interacao e ajustar método __str__

--- entidades/interacao.py
+++ entidades/interacao.py
@@ -9,7 +9,6 @@ class Interacao:
         self.__conteudo_associado: Conteudo = conteudo_associado
         self.__plataforma_interacao: Plataforma = plataforma_interacao
 
-        self.__interacao_id: int = int(linha_csv['interacao_id']) if 'interacao_id' in linha_csv else -1
         self.__id_usuario: int = int(linha_csv['id_usuario'])
         self.__timestamp_interacao: datetime.datetime = datetime.datetime.strptime(
             linha_csv['timestamp_interacao'], "%Y-%m-%d %H:%M:%S"
@@ -28,7 +27,6 @@ class Interacao:
     def __str__(self):
         return f"""Conteúdo associado: {self.conteudo_associado}
         Plataforma de interação: {self.plataforma_interacao}
-        Interação ID: {self.interacao_id}
         ID usuário: {self.id_usuario}
         Timestamp da interação: {self.timestamp_interacao}
         Tipo de interação: {self.tipo_interacao}
@@ -46,10 +44,6 @@ class Interacao:
     @property
     def plataforma_interacao(self) -> Plataforma:
         return self.__plataforma_interacao
-
-    @property
-    def interacao_id(self) -> int:
-        return self.__interacao_id
 
     @property
     def id_usuario(self) -> int:
