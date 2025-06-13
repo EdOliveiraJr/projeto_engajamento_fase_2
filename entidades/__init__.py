461d817f3ec8eb094cb33f7fe81735ee67fd0899
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2025-06-12 22:42:09 -0300

    feat: includes the Video, Podcast, Artigo classes on the __int__.py of the entidades file

--- entidades/__init__.py
+++ entidades/__init__.py
@@ -2,5 +2,8 @@ from entidades.plataforma import Plataforma
 from entidades.conteudo import Conteudo
 from entidades.usuario import Usuario
 from entidades.interacao import Interacao
+from entidades.video import Video
+from entidades.podcast import Podcast
+from entidades.artigo import Artigo
 
-__all__ = ['Plataforma', 'Conteudo', 'Usuario', 'Interacao']
+__all__ = ['Plataforma', 'Conteudo', 'Usuario', 'Interacao', 'Video', 'Podcast', 'Artigo']
