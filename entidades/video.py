9eaa840e2e6f36f3c7ecea0bb9b3bea8aaa5eef8
malufazenda <malu.fazenda@gmail.com>
2025-06-13 18:40:29 -0300

    referenciando watch duration seconds

--- entidades/video.py
+++ entidades/video.py
@@ -1,9 +1,9 @@
 from entidades import conteudo
 
 class Video(conteudo):
-    def __init__(self, id_conteudo, nome_conteudo, duracao_total_video_seg):
+    def __init__(self, id_conteudo, nome_conteudo, linha_csv: dict):
         super().__init__(id_conteudo, nome_conteudo)
-        self.__duracao_total_video_seg: int = duracao_total_video_seg
+        self.__duracao_total_video_seg: int = linha_csv["watch_duration_seconds"]
 
     @property
     def duracao_total_video_seg(self) -> int:
